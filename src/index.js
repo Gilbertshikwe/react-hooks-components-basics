1b030f57e857e0668d3870a56e0d8bf8bf959ffa
Gilbert <shikweg@gmail.com>
2024-01-31 08:17:25 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -2,4 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+
+function Article() {
+    return (
+      <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+    );
+  }
+  function Comment() {
+    return <div>Naturally, I agree with this article.</div>;
+
+}
 ReactDOM.render(<App />, document.getElementById("root"));
